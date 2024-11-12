8b3257c2ed03d378a83906a481bc8d3ed8bd0f5e
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-11-12 21:25:46 +0100

    Add player object

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ function GameBoard() {
     const board = new Array(9).fill("");
     return { board };
 }
+
+// create players
+const Player = () => {
+    const player1 = {
+        name: 'Player 1',
+        marker: 'X',
+    }
+    const player2 = {
+        name: 'Player 2',
+        marker: 'O',
+    }
+    return { player1, player2 }
+}
